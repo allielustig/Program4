6df376f36858c9a6086b6fb2ae0e90ce24338bbf
Allie Lustig <allielustig@gmail.com>
2016-05-31 18:21:40 -0700

    added open and close

--- libDisk.c
+++ libDisk.c
@@ -2,3 +2,31 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+int openDisk(char *filename, int nBytes){
+	int diskNum = -1;
+
+	if (nBytes > 0){
+		diskNum = open(filename, O_RDWR | O_CREAT);
+	}
+
+	else if (nBytes == 0){
+		diskNum = open(filename, O_RDWR);
+	}
+
+	return diskNum;
+}
+
+
+int readBlock(int disk, int bNum, void *block){
+
+}
+
+
+void closeDisk(int disk){
+	close(disk);
+}
